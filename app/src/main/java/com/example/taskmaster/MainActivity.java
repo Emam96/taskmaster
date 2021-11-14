a7ed22e625d094bc717f272f7a959785c922656d
Emam96 <emamsh990@gmail.com>
2021-11-14 16:52:27 +0200

    almost finished

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,10 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button ButtonOne = (Button) findViewById(R.id.buttonone);
+        Button ButtonTwo = (Button) findViewById(R.id.buttontwo);
+
+        ButtonOne.setOnClickListener(new  View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, Add.class));
+
+            }
+        });
+
+        ButtonTwo.setOnClickListener(new  View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this, All.class));
+            }
+        });
+
     }
 
-
-
-
-
 }
+
--- app/src/main/java/com/example/taskmaster/add.java
+++ app/src/main/java/com/example/taskmaster/add.java
@@ -2,13 +2,27 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class add extends AppCompatActivity {
+public class Add extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        Button buttonthree = (Button) findViewById(R.id.buttonthree);
+
+        buttonthree.setOnClickListener(new  View.OnClickListener(){
+            public void onClick(View v){
+
+                Toast.makeText(getApplicationContext(), "Task Added",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
